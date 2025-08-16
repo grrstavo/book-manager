80f2e3d2c4cc9240b8786c3e9bd2ea444f592338
Gustavo <gu1804@hotmail.com>
2025-08-16 20:36:59 -0300

    Add factories for Autor, Assunto, and Livro models to facilitate database seeding

---
+++ database/factories/LivroFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Livro>
+ */
+class LivroFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'Titulo' => fake()->unique()->text(maxNbChars: 40),
+            'Editora' => fake()->company(),
+            'Edicao' => fake()->numberBetween(1, 10),
+            'AnoPublicacao' => fake()->year(max: 'now'),
+            'Valor' => fake()->randomNumber(3)
+        ];
+    }
+}
