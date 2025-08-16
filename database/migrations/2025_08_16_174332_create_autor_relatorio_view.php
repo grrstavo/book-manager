97019a28bd61bbe6c78f581c94a8a8ec11aaf27d
Gustavo <gu1804@hotmail.com>
2025-08-16 15:45:30 -0300

    Add migration to create vw_relatorio_autor view for author report

---
+++ database/migrations/2025_08_16_174332_create_autor_relatorio_view.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement(
+            'CREATE VIEW vw_relatorio_autor AS
+            SELECT 
+                a.CodAu AS autor_id,
+                a.Nome AS autor_nome,
+                COUNT(DISTINCT l.Codl) AS total_livros,
+                SUM(l.Valor) AS total_valor,
+                COUNT(DISTINCT las.Assunto_codAs) AS total_assuntos
+            FROM Autor a
+            JOIN Livro_Autor la ON a.CodAu = la.Autor_CodAu
+            JOIN Livro l ON la.Livro_Codl = l.Codl
+            LEFT JOIN Livro_Assunto las ON l.Codl = las.Livro_Codl
+            GROUP BY a.CodAu, a.Nome;'
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vw_relatorio_autor');
+    }
+};
