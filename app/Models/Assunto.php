34686eb561bad3ed439bed84a4e77c65485fbbb6
Gustavo <gu1804@hotmail.com>
2025-08-16 20:38:19 -0300

    Add HasFactory trait to Autor, Assunto, and Livro models for factory support

--- app/Models/Assunto.php
+++ app/Models/Assunto.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Assunto extends Model
 {
+    use HasFactory;
+
     protected $table = 'Assunto';
 
     public $timestamps = false;
